5c34036867bcc4ea0d11bcf21cfebd12f9550a75
ananchevegor <84960978+ananchevegor@users.noreply.github.com>
2021-09-22 20:33:23 +0300

    Update Profile.dart

--- shpl/lib/Profile.dart
+++ shpl/lib/Profile.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({Key? key, this.backgroundImage}) : super(key: key);
-
-  final ImageProvider? backgroundImage;
+  const Profile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,10 +17,26 @@ class Profile extends StatelessWidget {
               Padding(padding: EdgeInsets.symmetric(vertical: 30, horizontal: 65)),
               CircleAvatar(
                 backgroundImage: NetworkImage('https://klike.net/uploads/posts/2019-05/1558767867_24.jpg'),
-                radius: 35,
-              )
+                radius: 45,
+              ),
             ],
-          )
+          ),
+          Column(
+            children: [
+              Padding(padding: EdgeInsets.symmetric(vertical: 38)),
+              Text("Username", style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                fontSize: 20
+              ),
+              ),
+              Padding(padding: EdgeInsets.symmetric(vertical: 5)),
+              Text("Phone: +79315862487", style: TextStyle(
+                fontWeight: FontWeight.w300,
+                fontSize: 15
+              ),
+              ),
+            ],
+          ),
         ],
       ),
     );
